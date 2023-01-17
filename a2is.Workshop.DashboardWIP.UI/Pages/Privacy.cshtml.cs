fbf88d26742eda780930df8bf3b02fae1f3734b8
Leonardo Cahaya <lcahaya@asuransiastra.com>
2023-01-17 12:46:54 +0700

    Add project files.

---
+++ a2is.Workshop.DashboardWIP.UI/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace a2is.Workshop.DashboardWIP.UI.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
